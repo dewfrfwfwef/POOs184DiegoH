93e95c47ffae44dad5ef220e90d8d06b8810b000
dewfrfwfwef <125634009+dewfrfwfwef@users.noreply.github.com>
2023-03-01 16:20:58 -0600

    Tkinter,: funciones con botones
    se modifico en clase

--- tkinter/practica11.py
+++ tkinter/practica11.py
@@ -1,7 +1,20 @@
 from tkinter import Tk,Button,Frame,messagebox
 
+#5. Funcion agregar mensajes
 def mostrarMensaje():
     messagebox.showinfo("aviso:","presionaste el boton azul")
+    messagebox.showerror("Error:","el proceso fallo con exito")
+    print(messagebox.askokcancel("Pregunta","Elle jugo con tu corazon"))
+    print(messagebox.askquestion("Pregunta rapida","¿eri gay?"))
+    print(messagebox.askyesnocancel("HI","hola"))
+    print(messagebox.askretrycancel("Que es esto!!!","no se que hago :v"))
+    
+#6. Funcion para agregar botones
+def agregarBoton():
+    btonVerde.config(text="+",bg="green",fg="white")
+    btonNuevo = Button(seccion3,text="Nuevo")
+    btonNuevo.pack()
+    
 
 #1. instanciamos el objeto ventana
 ventana= Tk()
@@ -28,10 +41,11 @@ btonAmarillo.grid(row= 1, column= 1)
 btonNegro= Button(seccion2,text="boton negro",fg="white",bg="black")
 btonNegro.grid(row= 0, column= 0)
 
-btonVerde= Button(seccion3,text="boton verde",fg="green",bg="#800080")
+btonVerde= Button(seccion3,text="boton verde",fg="green",bg="#800080",command=agregarBoton)
 btonVerde.configure(height=2,width=10)
 btonVerde.pack()
 
+#4. pocicion de elementos
 
 # llamamos al main
 ventana.mainloop()
